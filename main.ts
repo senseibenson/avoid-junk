04bc8d242b0bdc7854ac6d5737d00b038b00b837
senseibenson <73623987+senseibenson@users.noreply.github.com>
2021-02-19 18:07:10 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,156 @@
+controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
+    mySprite.setImage(faceLeft)
+})
+controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
+    mySprite.setImage(faceRight)
+})
+function makeEnemy () {
+    junkFood = sprites.create(JunkFoods._pickRandom(), SpriteKind.Enemy)
+    junkFood.vx = 40
+    junkFood.vy = 50
+    junkFood.y = 0
+    junkFood.x = randint(0, 160)
+    junkFood.setFlag(SpriteFlag.BounceOnWall, true)
+    junkFood.ay = 90
+}
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSprite) {
+    info.changeLifeBy(-1)
+    otherSprite.destroy()
+    sprite.say(messages._pickRandom(), 2000)
+})
+let junkFood: Sprite = null
+let mySprite: Sprite = null
+let faceRight: Image = null
+let faceLeft: Image = null
+let JunkFoods: Image[] = []
+let messages: string[] = []
+messages = ["I am not Hungry", "No Junk Food", "I am on diet"]
+JunkFoods = [img`
+    ..........bbbbbb................
+    .......bbb444444bb..............
+    .....2244444ddd444b.............
+    ....244444444dddd44e............
+    ...244444444444ddd4be...........
+    ..244444444444444d44be..........
+    .2b444444444444444d4be..........
+    .2b44444444444444444bbe.........
+    2bbb4444444444444444bbe.........
+    2bbb4444444444444444bbe.........
+    2bb4b4444444444444444bbe........
+    2bb4444444444444444444be........
+    2bb44444444444444444444e........
+    2bbb444bbb4444444444444e........
+    22bbb444bb4bb444444444be........
+    .2bbbbb44bbbb44444444bbe........
+    .22bbbbbbbb44bbb444444bbe.......
+    ..eeebbbbbbb44bbb444444be.......
+    ...eeeeebbbbbbbb44b4444be.......
+    .....eeeeee222bb44bbb4bbe.......
+    .......eeeee222bb44bbbbee.......
+    ............e222bbbbbbbec.......
+    ..............ee2bbbbeebdb......
+    .................eeeeecdddb.....
+    .......................cd11bbbb.
+    ........................cd111dbb
+    .........................b11111c
+    .........................c11dd1c
+    .........................cd1dbc.
+    .........................cb11c..
+    ..........................ccc...
+    ................................
+    `, img`
+    . . . . . . b b b b . . . . . . 
+    . . . . . . b 4 4 4 b . . . . . 
+    . . . . . . b b 4 4 4 b . . . . 
+    . . . . . b 4 b b b 4 4 b . . . 
+    . . . . b d 5 5 5 4 b 4 4 b . . 
+    . . . . b 3 2 3 5 5 4 e 4 4 b . 
+    . . . b d 2 2 2 5 7 5 4 e 4 4 e 
+    . . . b 5 3 2 3 5 5 5 5 e e e e 
+    . . b d 7 5 5 5 3 2 3 5 5 e e e 
+    . . b 5 5 5 5 5 2 2 2 5 5 d e e 
+    . b 3 2 3 5 7 5 3 2 3 5 d d e 4 
+    . b 2 2 2 5 5 5 5 5 5 d d e 4 . 
+    b d 3 2 d 5 5 5 d d d 4 4 . . . 
+    b 5 5 5 5 d d 4 4 4 4 . . . . . 
+    4 d d d 4 4 4 . . . . . . . . . 
+    4 4 4 4 . . . . . . . . . . . . 
+    `, img`
+    . . . . . . . . . . b 5 b . . . 
+    . . . . . . . . . b 5 b . . . . 
+    . . . . . . b b b b b b . . . . 
+    . . . . . b b 5 5 5 5 5 b . . . 
+    . . . . b b 5 d 1 f 5 5 d f . . 
+    . . . . b 5 5 1 f f 5 d 4 c . . 
+    . . . . b 5 5 d f b d d 4 4 . . 
+    . b b b d 5 5 5 5 5 4 4 4 4 4 b 
+    b d d d b b d 5 5 4 4 4 4 4 b . 
+    b b d 5 5 5 b 5 5 5 5 5 5 b . . 
+    c d c 5 5 5 5 d 5 5 5 5 5 5 b . 
+    c b d c d 5 5 b 5 5 5 5 5 5 b . 
+    . c d d c c b d 5 5 5 5 5 d b . 
+    . . c b d d d d d 5 5 5 b b . . 
+    . . . c c c c c c c c b b . . . 
+    . . . . . . . . . . . . . . . . 
+    `, img`
+    ..............bbbbbbb...........
+    ...........bb66663333baa........
+    .........bb3367776333663aa......
+    ........b33333888333389633aa....
+    .......b3333333333333389633aa...
+    ......b34443333333333338633bae..
+    .....b3455433333333334443333ae..
+    ....b33322333dddd3333455233daee.
+    ...b3d333333dd3bbbb33322333dabe.
+    ..b3d333333d3bb33bb33333333da4e.
+    ..bd33333333b33aab3333333223a4ee
+    .b3d3663333b33aab33366332442b4ee
+    .bd3b983333a3aa3333387633ee3b4ee
+    .bd6983333baaa333333387633bb4bee
+    b3d6833333bba333333333863ba44ebe
+    bdd3333333bb3333333333333a44bebe
+    add666633333322333366333ba44bbbe
+    ad67776333332442336983d3a444b4e.
+    add888b333333ee3369833d3a44b44e.
+    add333333333333336833d3a444b4e..
+    a3dd3333344433333dddd3a444b44e..
+    ab33ddd325543333dd33aa444b44e...
+    .eabb3dd32233333baaa4444b44e....
+    .ebabb3d333d33baa444443b44e.....
+    ..ebaab3ddd3aaa4444433b44e......
+    ..eebbaab33a44444333b444e.......
+    ...eeebbaab444b333b4444e........
+    ....ebeeebbbbbbbb4444ee.........
+    .....eebbbb44444444ee...........
+    .......eeebbb444eee.............
+    ..........eeeeee................
+    ................................
+    `]
+faceLeft = img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . c c c c c c . . . 
+    . . . . . . c 5 5 5 5 5 c c . . 
+    . . . . . c 5 5 5 5 5 5 5 5 c . 
+    . . . . c b b b b b b 5 5 5 c . 
+    . . . . c b b b b 1 b b c c . . 
+    . . . . c 1 1 b b 1 1 1 c . . . 
+    . . . c 1 1 1 1 b 1 1 1 c . . . 
+    . . . c 1 1 1 1 b 1 1 1 b b c c 
+    . . c c d 1 1 1 b 1 b 1 5 5 5 c 
+    . . c c d 1 c 1 1 1 b 1 b b 5 c 
+    . c c d d 1 1 1 1 1 b 1 f b 5 c 
+    f d d d 1 1 1 1 1 b b b f . c c 
+    f f f f f 1 1 1 b b 5 5 5 f . . 
+    . . . . . f f f 5 5 5 5 5 f . . 
+    . . . . . . . . f f f f f f . . 
+    `
+faceRight = faceLeft.clone()
+faceRight.flipX()
+mySprite = sprites.create(faceLeft, SpriteKind.Player)
+mySprite.y = 110
+controller.moveSprite(mySprite, 150, 0)
+mySprite.setStayInScreen(true)
+info.setLife(10)
+game.onUpdateInterval(2000, function () {
+    makeEnemy()
+})
